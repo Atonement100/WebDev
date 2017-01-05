7487044283294e42f6d754f09846ebd0582983b7
Timothy Russell-Wagner <t.wagner@ufl.edu>
2017-01-04 22:59:39 -0500

    move js from inline

---
+++ playground/windowController.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    vn = new VisiNeatAPI();
+    vn.setScreen("windowDiv");
+
+    var windowManager = vn.getWindowManager();
+    output = windowManager.createConsole({left:0,top:50,width:1000,height:800,title:"metreex analysis"});
+
+    output.println("test");
+
+
+    var nm = new NodeMetric('number of nodes');
+
+
+    var tree = new TreebankFile();
+    tree.onload = function(){
+        console.log(tree.getTitle());
+        output.println(tree.getTitle());
+    };
+
+    tree.load('wuxmwtm934d917x2');
+
+};
