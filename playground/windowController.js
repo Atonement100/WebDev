8627517f424535c79a12898825c0ad07653dd778
Timothy Russell-Wagner <t.wagner@ufl.edu>
2017-01-06 00:18:41 -0500

    More edge case handling for loading files by id

--- playground/windowController.js
+++ playground/windowController.js
@@ -1,24 +1,31 @@
 var loadedTreebanks = new Array();
 
+function AddLoadedTree(newTree){
+    loadedTreebanks.push(newTree);
+}
+
 VNConsoleWindow.prototype.processCommand=function(command){
     console.log("Command received in window controller: " + command);
-    output.println(command);
 
     var args = command.split(" ");
     switch (args[0].toLowerCase()){
         case "help":
-            output.println("Welcome to the metreex analysis tool, type 'load \<<metreex id\>>' to load a Treebank file.");
+            output.println("Welcome to the metreex analysis tool, type 'load &lt;metreex id>' to load a Treebank file.");
             output.println("Type list to see all loaded treebanks.");
             return true;
         case "load":
-            if (args.length > 1) {
-                output.println("loading " + args[1]);
+            if (args.length > 1 && args[1]) {
+                output.println("Attempting to load tree with id " + args[1]);
                 var newTree = new TreebankFile();
+                newTree.onload = function() {
+                    if (newTree.getNumOfSentences() > 0){
+                        AddLoadedTree(newTree);
+                    }
+                };
                 newTree.load(args[1]);
-                output.println(loadedTreebanks.push(newTree));
             }
             else{
-                output.println("Needs more parameters");
+                output.println("Not enough parameters. Usage: load &lt;metreex database id>");
             }
             return true;
         case "list":
